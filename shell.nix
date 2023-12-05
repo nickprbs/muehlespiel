23631a7dc3bb0bcbc81e49df7848d857c8d87f40
uowis <mail@example.com>
2023-12-05 11:55:25 +0100

    set up clean project

---
+++ shell.nix
@@ -0,0 +1,13 @@
+let
+  pkgs =
+    import <nixpkgs> {};
+  rust-toolchain = pkgs.symlinkJoin {
+    name = "rust-toolchain";
+    paths = [pkgs.rustc pkgs.cargo pkgs.rustPlatform.rustcSrc];
+  };
+in with pkgs;
+mkShell {
+  name = "scriptr";
+  buildInputs = [rust-toolchain];
+  RUST_BACKTRACE = 1;
+}
