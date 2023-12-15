2382ecd2608e1d452e5c7ded0750725ff1a5729a
uowis <mail@example.com>
2023-12-15 15:24:46 +0100

    delete old iterato

--- src/iterators/mod.rs
+++ src/iterators/mod.rs
@@ -3,12 +3,13 @@ mod lost_positions_iterator;
 mod location_iterator;
 mod mill_iterator;
 mod lost_positions_by_pieces_taken_iterator;
-mod lost_positions_by_cant_move_iterator;
 mod n_range_locations_iterator;
 mod neighbours_iterator;
 
 pub use crate::iterators::{
     board_equivalence_class_iterator::BoardEquivalenceClassIterator as BoardEquivalenceClassIterator,
-    lost_positions_by_cant_move_iterator::LostPositionsByCantMoveIterator as LostPositionsByCantMoveIterator,
     lost_positions_by_pieces_taken_iterator::LostPositionsByPiecesTakenIterator as LostPositionsByPiecesTakenIterator,
+    n_range_locations_iterator::NRangeLocationsIterator as NRangeLocationsIterator,
+    n_range_locations_iterator::NLocationsIterator as NLocationsIterator,
+    neighbours_iterator::NeighboursIterator as NeighboursIterator
 };
