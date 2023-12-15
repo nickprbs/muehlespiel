3861738b1628c8456f8500c523acc12482f99c9e
uowis <mail@example.com>
2023-12-15 14:36:07 +0100

    fix function signatures

--- src/producer/lost_positions/by_cant_move.rs
+++ src/producer/lost_positions/by_cant_move.rs
@@ -1,6 +1,6 @@
 use fnv::FnvHashSet;
-use crate::datastructures::GameBoard;
+use crate::datastructures::{GameBoard, Team};
 
-pub fn lost_positions_by_cant_move() -> FnvHashSet<GameBoard> {
+pub fn lost_positions_by_cant_move(loosing_team: Team) -> FnvHashSet<GameBoard> {
     todo!()
 }
--- src/producer/lost_positions/by_pieces_taken.rs
+++ src/producer/lost_positions/by_pieces_taken.rs
@@ -1,6 +1,6 @@
 use fnv::FnvHashSet;
-use crate::datastructures::GameBoard;
+use crate::datastructures::{GameBoard, Team};
 
-pub fn lost_positions_by_pieces_taken() -> FnvHashSet<GameBoard> {
+pub fn lost_positions_by_pieces_taken(loosing_team: Team) -> FnvHashSet<GameBoard> {
     todo!()
 }
