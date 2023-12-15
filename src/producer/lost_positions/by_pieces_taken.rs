b15b5cc81563fd7882e347d7789ef8bb894c95d7
nickprbs <np@rushsoft.de>
2023-12-15 15:22:15 +0100

    fixed from positions oder so again frfr

--- src/producer/lost_positions/by_pieces_taken.rs
+++ src/producer/lost_positions/by_pieces_taken.rs
@@ -2,5 +2,11 @@ use fnv::FnvHashSet;
 use crate::datastructures::{GameBoard, Team};
 
 pub fn lost_positions_by_pieces_taken(loosing_team: Team) -> FnvHashSet<GameBoard> {
-    todo!()
+    let output = FnvHashSet::default(); 
+    let initial_board: GameBoard = [0,0,0]; 
+    //initial buildup of all possible !canonical! boards where loser has two stones and winner has at 
+    //least one mill 
+    
+
+    output  
 }
