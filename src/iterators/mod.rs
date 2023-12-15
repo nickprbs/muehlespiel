7489e1879b5bccabd7f4c93a11f6eaeb818f7311
uowis <mail@example.com>
2023-12-15 13:46:07 +0100

    wip: lost by cant move

--- src/iterators/mod.rs
+++ src/iterators/mod.rs
@@ -5,6 +5,7 @@ mod mill_iterator;
 mod lost_positions_by_pieces_taken_iterator;
 mod lost_positions_by_cant_move_iterator;
 mod n_range_locations_iterator;
+mod neighbours_iterator;
 
 pub use crate::iterators::{
     board_equivalence_class_iterator::BoardEquivalenceClassIterator as BoardEquivalenceClassIterator,
