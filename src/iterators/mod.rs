f3b72a7d2475db57b235d208d237778054a435d5
uowis <mail@example.com>
2024-01-09 12:50:53 +0100

    fix build

--- src/iterators/mod.rs
+++ src/iterators/mod.rs
@@ -7,7 +7,7 @@ mod neighbours_iterator;
 
 pub use crate::iterators::{
     board_equivalence_class_iterator::BoardEquivalenceClassIterator as BoardEquivalenceClassIterator,
-    child_turn_iterator::ChildTurnIterator as TurnIterator,
+    child_turn_iterator::ChildTurnIterator as ChildTurnIterator,
     n_range_locations_iterator::NRangeLocationsIterator as NRangeLocationsIterator,
     n_range_locations_iterator::NLocationsIterator as NLocationsIterator,
     neighbours_iterator::NeighboursIterator as NeighboursIterator,
