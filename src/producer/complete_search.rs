5d5907f3dc11da936ad72b4059a8bf880d42dda5
uowis <mail@example.com>
2024-01-18 16:02:00 +0100

    improve performance

--- src/producer/complete_search.rs
+++ src/producer/complete_search.rs
@@ -36,8 +36,6 @@ fn mark_lost(states:  FnvHashSet<CanonicalGameBoard>, team: Team, lost_states: &
             if !lost_states.contains(state) {
                 lost_states.insert(*state);
                 for prev_state in ParentBoardIterator::new(team, *state) {
-                    //eprintln!("new parent state found");
-                    //eprintln!("");
                     if prev_state.get_num_pieces(Team::WHITE) <= MAX_NUM_PIECES_PER_TEAM && prev_state.get_num_pieces(Team::BLACK) <= MAX_NUM_PIECES_PER_TEAM {
                         possible_won_states.insert(prev_state);
                     }
@@ -52,38 +50,33 @@ fn mark_lost(states:  FnvHashSet<CanonicalGameBoard>, team: Team, lost_states: &
 
 fn mark_won(states:  FnvHashSet<CanonicalGameBoard>, team: Team, lost_states: &mut FnvHashSet<CanonicalGameBoard>, won_states: &mut FnvHashSet<CanonicalGameBoard>) {
     if !states.is_empty() {
-    let mut possible_lost_states: FnvHashSet<CanonicalGameBoard> = FnvHashSet::default();
-    for state in states.iter() {
-        if state.get_num_pieces(Team::WHITE) <= MAX_NUM_PIECES_PER_TEAM && state.get_num_pieces(Team::BLACK) <= MAX_NUM_PIECES_PER_TEAM {
-            if !won_states.contains(state) {
-                won_states.insert(*state);
+        let mut possible_lost_states: FnvHashSet<CanonicalGameBoard> = FnvHashSet::default();
+        let mut prev_states = FnvHashSet::default();
 
-                for prev_state in ParentBoardIterator::new(team, *state) {
-                    let mut child_iter = ChildTurnIterator::new(Phase::MOVE, team.get_opponent(), prev_state);
-                    if child_iter.all(|child_turn|
-                        {
-                            let mut child_board = prev_state.apply(child_turn, team.get_opponent());
-                            if child_board != *state {
-                                 child_board = child_board.get_representative();
-                            }
+        for state in states.iter() {
+            if state.get_num_pieces(Team::WHITE) <= MAX_NUM_PIECES_PER_TEAM && state.get_num_pieces(Team::BLACK) <= MAX_NUM_PIECES_PER_TEAM {
+                if !won_states.contains(state) {
+                    won_states.insert(*state);
 
-                            //if won_states.contains(&GameBoard::decode(String::from("EEBEEEEWWEEEEEEBWEEEBEEE")).get_representative()){
-                            //    eprintln!("won states contains 1st canonical!");
-                            //} else if won_states.contains(&GameBoard::decode(String::from("WEEEEEEEEEBEBEEEWEEWEEEB")).get_representative()){
-                            //    eprintln!("won states contains 2nd canonical!");
-                            //} else if won_states.contains(&GameBoard::decode(String::from("WBEEEEEEEEEEEEWEWEBEEEEB")).get_representative()){
-                            //    eprintln!("won states contains 3rd canonical!") 
-                            //}
-                            won_states.contains(&child_board)  
-                        }) {
-                        possible_lost_states.insert(prev_state);
-                    }
+                    ParentBoardIterator::new(team, *state)
+                        .for_each(|prev_state| { prev_states.insert(prev_state); });
                 }
             }
         }
-    }
-    eprintln!("executing mark_lost, input hash len:{}", possible_lost_states.len()); 
-    mark_lost( possible_lost_states, team.get_opponent(), lost_states, won_states);
+
+        for prev_state in prev_states {
+            let mut child_iter = ChildTurnIterator::new(Phase::MOVE, team.get_opponent(), prev_state);
+            if child_iter.all(|child_turn|
+                {
+                    let child_board = prev_state.apply(child_turn, team.get_opponent()).get_representative();
+                    won_states.contains(&child_board)
+                }) {
+                possible_lost_states.insert(prev_state);
+            }
+        }
+
+        eprintln!("executing mark_lost, input hash len:{}", possible_lost_states.len());
+        mark_lost( possible_lost_states, team.get_opponent(), lost_states, won_states);
     }
 }
 
